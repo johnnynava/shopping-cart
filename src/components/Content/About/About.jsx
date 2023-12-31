b62cb95908ab8cfdeb88d2b1e65d4c2f5a5ecceb
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-12-31 00:32:32 +0000

    Finish script for Home page
    Left to do: header, footer and other content pages.
    
    Also style everything.

--- src/components/Content/About/About.jsx
+++ src/components/Content/About/About.jsx
@@ -1,3 +1,20 @@
+// { path: "/", element: <Home /> },
+// { path: "about", element: <About /> },
+// { path: "checkout", element: <Checkout /> },
+// { path: "product-detail", element: <ProductDetail /> },
+// { path: "winter-2079-collection", element: <Winter2079Collection /> },
+
+// export const ShopContext = createContext({
+//   selectedPage: "",
+//   setSelectedPage: () => {},
+//   selectedProduct: "",
+//   setSelectedProduct: () => {},
+//   shoppingCartNum: 0,
+//   totalPrice: 0,
+//   shoppingCartArray: [],
+//   dispatch: () => {},
+// });
+
 const About = () => {};
 
 export default About;
--- src/components/Content/Home/Home.jsx
+++ src/components/Content/Home/Home.jsx
@@ -1,7 +1,49 @@
 import collectionArray from "../../../collectionArray";
 import { ShopContext } from "../../../App";
 import { useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
 
-const Home = () => {};
+const Home = () => {
+  const { setSelectedPage, setSelectedProduct } = useContext(ShopContext);
+
+  useEffect(() => {
+    setSelectedPage("/");
+  }, [setSelectedPage]);
+
+  return (
+    <div className="content">
+      <div className="videoContainer">
+        <video autoPlay muted loop>
+          <source src="/src/assets/media/video.mp4" type="video/mp4"></source>
+        </video>
+        <Link to="winter-2079-collection">Winter 2079 Collection</Link>
+      </div>
+      <div className="discoverSection">
+        {collectionArray.map((item, index) => {
+          if (index < 3) {
+            return (
+              <div className="featured" key={"featured" + index}>
+                <img src={item.image}></img>
+                <p>{item.name}</p>
+                <Link
+                  to="product-detail"
+                  onClick={() => {
+                    setSelectedProduct(item.id);
+                  }}
+                >
+                  Discover
+                </Link>
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div className="homeAbout">
+        <img src="/src/assets/media/homeAbout.jpg"></img>
+        <Link to="about">About us</Link>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
