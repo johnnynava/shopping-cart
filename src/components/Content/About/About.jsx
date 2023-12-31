0b9a4b8ce13af33d389e375fe33f89c5995a495b
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-12-31 00:41:10 +0000

    Create script for About

--- src/components/Content/About/About.jsx
+++ src/components/Content/About/About.jsx
@@ -1,20 +1,25 @@
-// { path: "/", element: <Home /> },
-// { path: "about", element: <About /> },
-// { path: "checkout", element: <Checkout /> },
-// { path: "product-detail", element: <ProductDetail /> },
-// { path: "winter-2079-collection", element: <Winter2079Collection /> },
-
-// export const ShopContext = createContext({
-//   selectedPage: "",
-//   setSelectedPage: () => {},
-//   selectedProduct: "",
-//   setSelectedProduct: () => {},
-//   shoppingCartNum: 0,
-//   totalPrice: 0,
-//   shoppingCartArray: [],
-//   dispatch: () => {},
-// });
-
-const About = () => {};
+const About = () => {
+  return (
+    <div className="content">
+      <img src="/src/assets/media/about.webp"></img>
+      <p>About us</p>
+      <p>
+        At vero eos et accusamus et iusto odio dignissimos ducimus qui
+        blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
+        et quas molestias excepturi sint occaecati cupiditate non provident,
+        similique sunt in culpa qui officia deserunt mollitia animi, id est
+        laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
+        distinctio. Nam libero tempore, cum soluta nobis est eligendi optio
+        cumque nihil impedit quo minus id quod maxime placeat facere possimus,
+        omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem
+        quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet
+        ut et voluptates repudiandae sint et molestiae non recusandae. Itaque
+        earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+        voluptatibus maiores alias consequatur aut perferendis doloribus
+        asperiores repellat.
+      </p>
+    </div>
+  );
+};
 
 export default About;
--- src/components/Content/Checkout/Checkout.jsx
+++ src/components/Content/Checkout/Checkout.jsx
@@ -1,5 +1,22 @@
 import collectionArray from "../../../collectionArray";
 
+// { path: "/", element: <Home /> },
+// { path: "about", element: <About /> },
+// { path: "checkout", element: <Checkout /> },
+// { path: "product-detail", element: <ProductDetail /> },
+// { path: "winter-2079-collection", element: <Winter2079Collection /> },
+
+// export const ShopContext = createContext({
+//   selectedPage: "",
+//   setSelectedPage: () => {},
+//   selectedProduct: "",
+//   setSelectedProduct: () => {},
+//   shoppingCartNum: 0,
+//   totalPrice: 0,
+//   shoppingCartArray: [],
+//   dispatch: () => {},
+// });
+
 const Checkout = () => {};
 
 export default Checkout;
